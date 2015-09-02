8ecd9e13ec9785ec1e19c7b06b1e66d3d61af8af
snandi <nandi@stat.wisc.edu>
2015-09-02 18:22:52 -0500

    First boxplot of income poverty

--- RScripts_Recession/RScript02_sipp08_disab.R
+++ RScripts_Recession/RScript02_sipp08_disab.R
@@ -24,15 +24,77 @@ Today <- Sys.Date()
 ########################################################################
 Filepath1 <- paste(RDataPath, 'sipp08_MASTER.RData', sep = '')
 load(Filepath1)
+sipp08_master <- within(data = sipp08_master,{
+  epppnum <- as.integer(epppnum)
+})
 
 length(unique(as.numeric(sipp08_master$ssuid)))
 
-sipp08_01 <- subset(sipp08_master, ssuid == "019128000276")
+# sipp08_01 <- subset(sipp08_master, ssuid == "019133398883")
+# sipp08_02 <- subset(sipp08_master, ssuid %in% ssuids[1:20])
+# View(sipp08_01)
+
 ########################################################################
 ## 2008_disability.dta 
 ########################################################################
 Filepath2 <- paste(RDataPath, '2008_disability.RData', sep = '')
 load(Filepath2)
-
+ssuids <- unique(as.vector(disability_2008$ssuid))
 length(unique(as.numeric(disability_2008$ssuid)))
+
+Colnames_Keep_disab <- c('ssuid', 'shhadid', 'epppnum', 'disb_wrk_ageR2', 'gas', 'rent', 'meet_expenses', 
+                         'phone', 'medical')
+
+########################################################################
+## Get disability information, with only 1 type of disability entry 
+## per ssuid, in the disability_2008 data
+########################################################################
+Disab <- fn_returnDisb_ssuid(disbData = disability_2008)
+disability_2008_1 <- unique(Disab[['disability_2008_1']][,Colnames_Keep_disab])
+ssuid_disb_1 <- Disab[['ssuid_disb_1']]
+
+########################################################################
+## Merge sipp_2008 & 2008_disability.dta 
+########################################################################
+sipp08_master_disab <- merge(x = sipp08_master, y = disability_2008_1, 
+                             by = c('ssuid', 'shhadid', 'epppnum'), all.x = T)
+sipp08_master_disab$yearmon <- as.yearmon(paste(sipp08_master_disab$rhcalmn, sipp08_master_disab$rhcalyr))
+sipp08_master_disab <- sipp08_master_disab[order(sipp08_master_disab$ssuid, sipp08_master_disab$yearmon),]
+
+Colnames_Keep_merged <- c('ssuid', 'shhadid', 'yearmon', 'ehrefper', 'rhtype', 'whfnwgt', 'thtotinc', 'rhpov', 'epppnum',
+                          'disb_wrk_ageR2')
+sipp08_master_disab <- sipp08_master_disab[, Colnames_Keep_merged]
+
+# disab_01 <- subset(disability_2008, ssuid == "730925701502")[,Colnames_Keep_disab]
+sipp08_master_disab <- subset(sipp08_master_disab, ssuid %in% ssuid_disb_1[1:500])
+sipp08_master_disab <- na.omit(sipp08_master_disab)
+
+#View(sipp08_master_disab[,Colnames_Keep_merged])
+str(sipp08_master_disab)
+
+########################################################################
+## Get Income Poverty information
+########################################################################
+Data_forIncPov <- aggregate(cbind(thtotinc, rhpov, disb_wrk_ageR2) ~ ssuid + yearmon, 
+                            data = sipp08_master_disab, FUN = mean)
+Data_forIncPov <- Data_forIncPov[order(Data_forIncPov$ssuid, Data_forIncPov$yearmon), ]
+Data_forIncPov$rhpov2 <- 2 * Data_forIncPov$rhpov
+
+Data_forIncPov$FPL200 <- Data_forIncPov$thtotinc < Data_forIncPov$rhpov2
+Data_forIncPov$FPL100 <- Data_forIncPov$thtotinc < Data_forIncPov$rhpov
+Data_forIncPov$FPL200[Data_forIncPov$FPL100 == TRUE] <- FALSE
+
+Data_forIncPov$Pct_rhpov <- Data_forIncPov$thtotinc/Data_forIncPov$rhpov
+Data_forIncPov$disb_wrk_ageR2 <- factor(Data_forIncPov$disb_wrk_ageR2, labels = c('no', 'yes'))
+str(Data_forIncPov)
+
+qplot() + geom_boxplot(aes(x = as.factor(as.Date(yearmon)), y = Pct_rhpov), data = Data_forIncPov )
+qplot() + geom_boxplot(aes(x = as.factor(as.Date(yearmon)), y = Pct_rhpov, fill = disb_wrk_ageR2), 
+                       data = Data_forIncPov )
+
+qplot() + geom_line(aes(x = as.Date(yearmon), y = Pct_rhpov, col = ssuid), data = Data_forIncPov )
+
+str(Data_forIncPov)
+
+
 
--- RScripts_Recession/fn_Library_Recession.R
+++ RScripts_Recession/fn_Library_Recession.R
@@ -48,3 +48,22 @@ colVar <- function(Data){
 }
 ################################################################## 
 
+################################################################## 
+## Returns disability information, from disability_2008 dataset
+##################################################################
+fn_returnDisb_ssuid <- function(disbData = disability_2008){
+  
+  disb_only <- unique(disbData[,c('ssuid', 'disb_wrk_ageR2')])
+  disb_only$ssuid <- as.factor(disb_only$ssuid)
+  disb_only_table <- table(disb_only$ssuid)
+  ssuid_disb_1 <- names(disb_only_table[disb_only_table == 1])
+  ## ssuid_disb_1 is a list of ssuids that have only ONE disability status, per ssuid
+  ssuid_disb_2ORmore <- disb_only_table[disb_only_table > 1] 
+  #length(disb_only_table[disb_only_table > 1])
+  
+  return(list(disb_only_table = disb_only_table, 
+              disability_2008_1 = subset(disability_2008, ssuid %in% ssuid_disb_1), 
+              ssuid_disb_1 = ssuid_disb_1, 
+              ssuid_disb_2ORmore = ssuid_disb_2ORmore
+              ))
+}
