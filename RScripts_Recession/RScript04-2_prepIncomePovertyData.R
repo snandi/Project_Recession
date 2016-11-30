bbfdaca8b63ad27b865dcf1b2c302ba650ee2ea3
snandi <nandi@stat.wisc.edu>
2016-11-30 12:43:56 -0600

    Update income poverty data prep, with different weights, and negative totInc

--- RScripts_Recession/RScript04-2_prepIncomePovertyData.R
+++ RScripts_Recession/RScript04-2_prepIncomePovertyData.R
@@ -16,24 +16,23 @@ rm( list=objects( all.names=TRUE ) )
 ## Also, this one uses yearqtr instead of yearmon
 ##################################################################
 fn_DataforIncPov_v3 <- function( Data ){
-  Data <- Data[order( Data$ssuid, Data$yearqtr ), ]
-  Data$rhpov2 <- 2 * Data$rhpov
+  Data$rhpov2 <- 2 * Data$rhpov_qtr
   
-  Data$FPL200 <- Data$thtotinc < Data$rhpov2
-  Data$FPL100 <- Data$thtotinc < Data$rhpov
+  Data$FPL200 <- ( Data$thtotinc_qtr < Data$rhpov2 )
+  Data$FPL100 <- ( Data$thtotinc_qtr < Data$rhpov_qtr )
   Data$FPL200[Data$FPL100 == TRUE] <- FALSE
   
-  Data$Pct_rhpov <- Data$thtotinc/Data$rhpov
-  ## Data$disb_wrk_ageR2 <- factor( Data$disb_wrk_ageR2, labels = c( 'no', 'yes' ) )
+  Data$Pct_rhpov <- Data$thtotinc_qtr/Data$rhpov_qtr
+  
   Data$adult_disb <- factor( Data$adult_disb, labels = c( 'no', 'yes' ) )
   
   rownames( Data ) <- NULL
   #Data <- subset( Data, yearmon != 'May 2008' )
-  Data$FPL100_num <- Data$thtotinc/Data$rhpov
-  Data$FPL200_num <- Data$thtotinc/Data$rhpov2
+  Data$FPL100_num <- Data$thtotinc_qtr/Data$rhpov_qtr
+  Data$FPL200_num <- Data$thtotinc_qtr/Data$rhpov2
   
-  Data$FPL100_num[Data$FPL100_num == 0] <- 0.001
-  Data$FPL200_num[Data$FPL200_num == 0] <- 0.001
+#   Data$FPL100_num[Data$FPL100_num == 0] <- 0.001
+#   Data$FPL200_num[Data$FPL200_num == 0] <- 0.001
   
   ## Normalize FPL100
   SplitByssuid <- split( x = Data, f = as.factor( Data$ssuid ) )
@@ -43,25 +42,25 @@ fn_DataforIncPov_v3 <- function( Data ){
   ## Normalize FPL200
   Data$FPL200_noBaseline <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = normalize_baseline, Colname = 'FPL200_num' ) )
   
-  ## Lag FPL100
-  Data$FPL100_num_Lag <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = fn_lagData, Colname = 'FPL100_num' ) )
-  
-  ## Lag FPL200
-  Data$FPL200_num_Lag <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = fn_lagData, Colname = 'FPL200_num' ) )
-  
-  try( rm( SplitByssuid ) )
-  gc( )
-  
-  ## Lag FPL100_Norm
-  SplitByssuid <- split( x = Data, f = as.factor( Data$ssuid ) )
-  Data$FPL100_norm_Lag <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = fn_lagData, Colname = 'FPL100_noBaseline' ) )
-  rownames( Data ) <- NULL
-  
-  ## Lag FPL200_Norm
-  Data$FPL200_norm_Lag <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = fn_lagData, Colname = 'FPL200_noBaseline' ) )
+#   ## Lag FPL100
+#   Data$FPL100_num_Lag <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = fn_lagData, Colname = 'FPL100_num' ) )
+#   
+#   ## Lag FPL200
+#   Data$FPL200_num_Lag <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = fn_lagData, Colname = 'FPL200_num' ) )
   
   try( rm( SplitByssuid ) )
   gc( )
+  
+#   ## Lag FPL100_Norm
+#   SplitByssuid <- split( x = Data, f = as.factor( Data$ssuid ) )
+#   Data$FPL100_norm_Lag <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = fn_lagData, Colname = 'FPL100_noBaseline' ) )
+#   rownames( Data ) <- NULL
+#   
+#   ## Lag FPL200_Norm
+#   Data$FPL200_norm_Lag <- do.call( what = c, args = lapply( X = SplitByssuid, FUN = fn_lagData, Colname = 'FPL200_noBaseline' ) )
+#   
+#   try( rm( SplitByssuid ) )
+#   gc( )
   
   comment( Data ) <- 'The lower the value of Pct_rhpov, the worse off the household is'
   return( Data )
@@ -76,13 +75,10 @@ RScriptPath <- paste0( PathPrefix, 'Project_Recession/RScripts_Recession/' )
 DataPath <- paste0( PathPrefix, 'Project_Recession/Data/data_2015Dec/' )
 RDataPath <- paste0( PathPrefix, 'Project_Recession/RData/data_2015Dec/' )
 PlotPath <- paste0( PathPrefix, 'Project_Recession/Plots/' )
-Filename.Header <- paste0( PathPrefix, 'RScripts/HeaderFile_lmcg.R' )
-source( Filename.Header )
-
-library( dplyr )
+Filename.Header <- paste0(RScriptPath, 'HeaderFile_Recession.R')
+source(Filename.Header)
 
 source( paste( RScriptPath, 'fn_Library_Recession.R', sep='' ) )
-source( '~/RScripts/fn_Library_SN.R' )
 
 ########################################################################
 Today <- Sys.Date( )
@@ -219,17 +215,23 @@ summary( Data$whfnwgt )
 ########################################################################
 Data <- dplyr::arrange( .data = Data, ssuid, yearqtr, yearmon )
 
-View( subset( Data, ssuid == "955925986509" ) )
+#View( subset( Data, ssuid == "955925986509" ) )
 
-data_qtr <- ddply( .data = subset( Data, ssuid == "019128000276" ), 
-                      .( ssuid, shhadid, yearqtr, ms, gender, gender_ms, race_origin, education ), 
-                      function( x ) data.frame( cbind( thtotinc_qtr = weighted.mean( x$thtotinc, x$whfnwgt ), 
-                                                     whfnwgt_qtr = mean( x$whfnwgt ), 
-                                                     rhpov_qtr = mean( x$rhpov ), 
-                                                     adult_disb = max( x$adult_disb ) ) ) )
+nCores <- max( 1, detectCores() - 2 )
+cl <- parallel::makeCluster( nCores )
+doParallel::registerDoParallel( cl )
+# data_qtr <- ddply( .data = subset( Data, ssuid == "019128000276" ), 
+data_qtr <- ddply( .data = Data, .parallel = TRUE, 
+                   .( ssuid, shhadid, yearqtr, ms, gender, gender_ms, race_origin, education ), 
+                   function( x ) data.frame( cbind( thtotinc_qtr = weighted.mean( x$thtotinc, x$whfnwgt ), 
+                                                    whfnwgt_qtr = mean( x$whfnwgt ), 
+                                                    rhpov_qtr = mean( x$rhpov ), 
+                                                    adult_disb = max( x$adult_disb ) ) ) )
 
+stopCluster(cl)  
+gc()
 
-Data_forIncPov <- fn_DataforIncPov_v2( Data = data_qtr )
+Data_forIncPov <- fn_DataforIncPov_v3( Data = data_qtr )
 
 ########################################################################
 ## create a household id
@@ -239,8 +241,8 @@ Data_forIncPov$hhid <- paste( Data_forIncPov$ssuid, Data_forIncPov$shhadid, sep 
 ########################################################################
 ## save the data
 ########################################################################
-Filename <- paste0( RDataPath, 'Data_forIncPov_v4.RData' )
+Filename <- paste0( RDataPath, 'Data_forIncPov_v5_newWts.RData' )
 save( Data_forIncPov, file = Filename )
-rm( Temp, Data_forIncPov )
+try( rm( Data, Temp, Data_forIncPov ) )
 gc( )
 
---
+++ RScripts_Recession/RScript05-2_IncPovAnalysis.R
@@ -0,0 +1,292 @@
+rm(list = ls(all.names = TRUE))
+rm(list = objects(all.names = TRUE))
+#dev.off()
+
+########################################################################
+## This script analyzes the income poverty over time. This is similar to
+## RScript05, with the following differences
+## 1. This uses Data_forIncPov_v5_newWts.RData
+## 2. This does not fit log(FPL100)
+########################################################################
+
+########################################################################
+## Run Path definition file                                           ##
+########################################################################
+PathPrefix <- '~/'
+# PathPrefix <- '/Users/patron/Documents/snandi/'
+RScriptPath <- paste0(PathPrefix, 'Project_Recession/RScripts_Recession/')
+DataPath <- paste0(PathPrefix, 'Project_Recession/Data/data_2015Dec/')
+RDataPath <- paste0(PathPrefix, 'Project_Recession/RData/data_2015Dec/')
+PlotPath <- paste0(PathPrefix, 'Project_Recession/Plots/')
+Filename.Header <- paste0(RScriptPath, 'HeaderFile_Recession.R')
+source(Filename.Header)
+
+source(paste(RScriptPath, 'fn_Library_Recession.R', sep=''))
+source(paste(RScriptPath, 'plotLSMeans.R', sep=''))
+
+########################################################################
+Today <- Sys.Date()
+
+########################################################################
+## load income poverty data
+########################################################################
+#Filename <- paste0(RDataPath, 'Data_forIncPov_byRace.RData')
+#load(file = Filename)
+
+##Filename <- paste0(RDataPath, 'Data_forIncPov.RData')
+##load(file = Filename)
+Filename <- paste0(RDataPath, 'Data_forIncPov_v5_newWts.RData')
+load(file = Filename)
+
+Data <- Data_forIncPov
+Data$year <- substr(x = Data$yearqtr, start = 1, stop = 4)
+Data$year <- as.factor(Data$year)
+
+rm(Data_forIncPov)
+#View(Data[,c('hhid', 'yearqtr', 'thtotinc', 'rhpov', 'adult_disb', 'FPL100_num', 'FPL100_num_Lag')])
+
+#######################################################################
+## Mixed Effects Model (MEM) of Income Poverty Ratio
+########################################################################
+#library(lsmeans)
+
+Data$wt <- Data$wt/1000
+
+Num1_FPL100_wt <- lme4::lmer( FPL100_num ~ 1 + yearqtr + gender_ms + race + 
+                                    race*adult_disb + adult_disb + adult_disb*gender_ms + 
+                                    adult_disb*gender_ms + adult_disb*yearqtr + (1 | hhid), 
+                                  data = Data, weights = whfnwgt_qtr )
+
+summary(Num1_FPL100_wt)
+# lmerTest::anova(Num1_FPL100_wt_log) ## 
+# anova(Num1_FPL100_wt_log) ## 
+
+# Resid1_log <- as.data.frame(cbind(yearqtr = Data$yearqtr, resid = residuals(Num1_FPL100_wt_log)))
+# Resid1_logPlot <- qplot() + geom_point(aes(x = yearqtr, y = resid), data = Resid1_log)
+# xtable(anova(Num1_FPL100_wt_log))
+
+#######################################################################
+## Post hoc: Race
+#######################################################################
+PostHoc_race <- lmerTest::difflsmeans(
+  model =   Num1_FPL100_wt_log, 
+  test.effs = 'race'
+)
+Plot_race <- plotLSMeans(
+  response = PostHoc_race$response,
+  table = PostHoc_race$diffs.lsmeans.table, 
+  which.plot = 'DIFF of LSMEANS', 
+  mult = TRUE
+)
+Plot_race
+PlotFilename <- paste0(SlidePath, 'PlotPostHoc_', 'race', '.jpeg')
+ggsave(
+  filename = PlotFilename, 
+  plot = Plot_race, 
+  device = 'jpg', 
+  width = 30,
+  height = 20,
+  units = 'cm'
+)
+
+#######################################################################
+## Post hoc: gender_ms
+#######################################################################
+PostHoc_gender_ms <- lmerTest::difflsmeans(
+  model =   Num1_FPL100_wt_log, 
+  test.effs = 'gender_ms'
+)
+Plot_gender_ms <- plotLSMeans(
+  response = PostHoc_gender_ms$response,
+  table = PostHoc_gender_ms$diffs.lsmeans.table, 
+  which.plot = 'DIFF of LSMEANS', 
+  mult = TRUE
+)
+PlotFilename <- paste0(SlidePath, 'PlotPostHoc_', 'gender_ms', '.jpeg')
+ggsave(
+  filename = PlotFilename, 
+  plot = Plot_gender_ms, 
+  device = 'jpg', 
+  width = 40,
+  height = 30,
+  units = 'cm'
+)
+
+#######################################################################
+## Post hoc: Race & Disability
+########################################################################
+PostHoc_race_disb <- lmerTest::difflsmeans(
+  model =   Num1_FPL100_wt_log, 
+  test.effs = c('race:adult_disb')
+)
+str(PostHoc_race_disb)
+# Plot_gender_ms <- plot(PostHoc_gender_ms)
+Plot_race_disb <- plotLSMeans(
+  response = PostHoc_race_disb$response,
+  table = PostHoc_race_disb$diffs.lsmeans.table, 
+  which.plot = 'DIFF of LSMEANS', 
+  mult = TRUE
+)
+Plot_race_disb
+PlotFilename <- paste0(SlidePath, 'PlotPostHoc_', 'race_disb', '.jpeg')
+ggsave(
+  filename = PlotFilename, 
+  plot = Plot_race_disb, 
+  device = 'jpg', 
+  width = 40,
+  height = 30,
+  units = 'cm'
+)
+
+#######################################################################
+## Post hoc: Gender, MS & Disability
+########################################################################
+PostHoc_gender_ms_disb <- lmerTest::difflsmeans(
+  model =   Num1_FPL100_wt_log, 
+  test.effs = c('gender_ms:adult_disb')
+)
+# str(PostHoc_gender_ms_disb)
+# str(PostHoc_gender_ms_disb$diffs.lsmeans.table)
+# Plot_gender_ms_disb <- plot(PostHoc_gender_ms_disb, which.plot = 'DIFF of LSMEANS')
+Plot_gender_ms_disb <- plotLSMeans(
+  response = PostHoc_gender_ms_disb$response,
+  table = PostHoc_gender_ms_disb$diffs.lsmeans.table, 
+  which.plot = 'DIFF of LSMEANS', 
+  mult = TRUE
+)
+PlotFilename <- paste0(SlidePath, 'PlotPostHoc_', 'gender_ms_disb', '.jpeg')
+ggsave(
+  filename = PlotFilename, 
+  plot = Plot_gender_ms_disb, 
+  device = 'jpg', 
+  width = 40,
+  height = 30,
+  units = 'cm'
+)
+
+#######################################################################
+## Mixed Effects Model (MEM) of normalized FPL 100 
+#######################################################################
+Data$FPL100_noBaseline[Data$FPL100_noBaseline == 0] <- 0.001
+
+MEM2_FPL100_wt_log <- lme4::lmer(log(FPL100_noBaseline) ~ 1 + yearqtr + 
+                             yearqtr*adult_disb + race*adult_disb +
+                             race + gender_ms + adult_disb + wt + 
+                             adult_disb*gender_ms + (1 | hhid), 
+                       data = Data, REML = TRUE)
+summary(MEM2_FPL100_wt_log)
+#xtable(MEM2_FPL100_wt_log)
+
+#######################################################################
+## Post hoc: Race
+#######################################################################
+PostHoc_race <- lmerTest::difflsmeans(
+  model = MEM2_FPL100_wt_log, 
+  test.effs = 'race'
+)
+Plot_race_Norm <- plotLSMeans(
+  response = PostHoc_race$response,
+  table = PostHoc_race$diffs.lsmeans.table, 
+  which.plot = 'DIFF of LSMEANS', 
+  mult = TRUE
+)
+PlotFilename <- paste0(SlidePath, 'PlotPostHocNorm_', 'race', '.jpeg')
+ggsave(
+  filename = PlotFilename, 
+  plot = Plot_race_Norm, 
+  device = 'jpg', 
+  width = 40,
+  height = 30,
+  units = 'cm'
+)
+
+#######################################################################
+## Post hoc: gender_ms
+#######################################################################
+PostHoc_gender_ms <- lmerTest::difflsmeans(
+  model = MEM2_FPL100_wt_log, 
+  test.effs = 'gender_ms'
+)
+Plot_gender_ms <- plotLSMeans(
+  response = PostHoc_gender_ms$response,
+  table = PostHoc_gender_ms$diffs.lsmeans.table, 
+  which.plot = 'DIFF of LSMEANS', 
+  mult = TRUE
+)
+PlotFilename <- paste0(SlidePath, 'PlotPostHocNorm_', 'gender_ms', '.jpeg')
+ggsave(
+  filename = PlotFilename, 
+  plot = Plot_gender_ms, 
+  device = 'jpg', 
+  width = 40,
+  height = 30,
+  units = 'cm'
+)
+
+#######################################################################
+## Post hoc: Race & Disability
+#######################################################################
+PostHoc_race_disb <- lmerTest::difflsmeans(
+  model = MEM2_FPL100_wt_log, 
+  test.effs = c('adult_disb:race')
+)
+str(PostHoc_race_disb)
+# Plot_gender_ms <- plot(PostHoc_gender_ms)
+Plot_race_disb <- plotLSMeans(
+  response = PostHoc_race_disb$response,
+  table = PostHoc_race_disb$diffs.lsmeans.table, 
+  which.plot = 'DIFF of LSMEANS', 
+  mult = TRUE
+)
+Plot_race_disb
+PlotFilename <- paste0(SlidePath, 'PlotPostHocNorm_', 'race_disb', '.jpeg')
+ggsave(
+  filename = PlotFilename, 
+  plot = Plot_race_disb, 
+  device = 'jpg', 
+  width = 40,
+  height = 30,
+  units = 'cm'
+)
+
+#######################################################################
+## Post hoc: Gender, MS & Disability
+########################################################################
+PostHoc_gender_ms_disb <- lmerTest::difflsmeans(
+  model = MEM2_FPL100_wt_log, 
+  test.effs = c('adult_disb:gender_ms')
+)
+# str(PostHoc_gender_ms_disb)
+# str(PostHoc_gender_ms_disb$diffs.lsmeans.table)
+# Plot_gender_ms_disb <- plot(PostHoc_gender_ms_disb, which.plot = 'DIFF of LSMEANS')
+Plot_gender_ms_disb <- plotLSMeans(
+  response = PostHoc_gender_ms_disb$response,
+  table = PostHoc_gender_ms_disb$diffs.lsmeans.table, 
+  which.plot = 'DIFF of LSMEANS', 
+  mult = TRUE
+)
+PlotFilename <- paste0(SlidePath, 'PlotPostHocNorm_', 'gender_ms_disb', '.jpeg')
+ggsave(
+  filename = PlotFilename, 
+  plot = Plot_gender_ms_disb, 
+  device = 'jpg', 
+  width = 40,
+  height = 30,
+  units = 'cm'
+)
+
+#######################################################################
+## Residuals
+#######################################################################
+Data$Resid_Num1 <- residuals(Num1_FPL100_wt)
+Data$Resid_Num2 <- residuals(Num2_FPL100_wt) 
+Data$Resid_Num3 <- residuals(Num3_FPL100_wt)
+
+Data$Resid_MEM1 <- residuals(MEM1_FPL100_wt)
+Data$Resid_MEM2 <- residuals(MEM2_FPL100_wt)
+Data$Resid_MEM3 <- residuals(MEM3_FPL100_wt)
+
+Filename <- paste0(RDataPath, 'Data_forIncPov_v3_Residuals.RData')
+save(Data, file = Filename)
+
+
