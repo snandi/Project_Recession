c6a0b01a98c90fdec4cc493ecb4e57675c061190
snandi <nandi@stat.wisc.edu>
2017-03-12 19:59:03 -0500

    Running model with baseline only

--- RScripts_Recession/RScript05-3_IncPovAnalysis.R
+++ RScripts_Recession/RScript05-3_IncPovAnalysis.R
@@ -4,9 +4,8 @@ rm( list = objects( all.names = TRUE ) )
 
 ########################################################################
 ## This script analyzes the income poverty over time. This is similar to
-## RScript05, with the following differences
-## 1. This uses Data_forIncPov_v5_newWts.RData
-## 2. This does not fit log( FPL100 )
+## RScript05-2, with the following differences
+## 1. This analyzes FPL100Num, with baselines 
 ########################################################################
 preprocessAndSaveData <- function( Data, filenameModelData = 'Data_forIncPovModel_v5.RData' ){
   Data$year <- substr( x = Data$yearqtr, start = 1, stop = 4 )
@@ -50,23 +49,6 @@ formatPostHocTables <- function( postHocTable, multipleCorrection = TRUE,
   
   postHocTableDF <- postHocTableDF[ order( postHocTableDF$`p-value`, decreasing = F ), ]
   return( postHocTableDF )
-}
-
-mergePostHocTables <- function( postHoc1, postHoc2, multipleCorrection = TRUE ){
-  
-  ttplot1 <- formatPostHocTables( postHocTable = postHoc1, multipleCorrection = multipleCorrection )
-  ttplot2 <- formatPostHocTables( postHocTable = postHoc2, multipleCorrection = multipleCorrection )
-  
-  columnsToMerge <- c( 'Factor Levels', 'Estimate', 'Standard Error', 't-value', 'p-value' )
-  ttplot <- merge( x = ttplot1[,columnsToMerge], y = ttplot2[,columnsToMerge], 
-                   by = 'Factor Levels', all = T )  
-  
-  ttplot <- ttplot[ order( ttplot$`t-value.y`, ttplot$`t-value.x`, decreasing = T ), ]
-  colnames( ttplot ) <- c( 'Factor Levels', 'Est 1', 'Std Err 1', 't1', 'p-value 1', 
-                           'Est 2', 'Std Err 2', 't2', 'p-value 2' )
-  ttplot$t1 <- ttplot$t2 <- NULL
-  
-  return( ttplot )
 }
 
 ########################################################################
@@ -142,65 +124,38 @@ print( xtable( modelFPL100_AnovaDF, digits = c( 0, 2, 2, 0, 2, 4 ) ,
 ) )
 
 #######################################################################
-## Mixed Effects Model ( MEM ) of Income Poverty Ratio, controlled for 
-## Baseline value of FPL100
-########################################################################
-modelFPL100NoBaseline <- lmerTest::lmer( 
-  FPL100_noBaseline ~ 1 + Time + I( Time^2 ) + adult_disb + gender + ms + race_origin + education + 
-    adult_disb*Time +
-    gender*ms + gender*education + ms*race_origin + 
-    ( 1 | hhid ), data = Data, weights = wt 
-)
-
-modelFPL100NoBaseline_Summary <- lmerTest::summary( modelFPL100NoBaseline )
-print( modelFPL100NoBaseline_Summary )
-modelFPL100NoBaseline_Anova <- lmerTest::anova( modelFPL100NoBaseline )
-
-time3 <- Sys.time( )
-print( time3 - time2 )
-
-modelFPL100NoBaseline_AnovaDF <- formatAnovaTableForXtable( anovaTable = modelFPL100NoBaseline_Anova )
-print( xtable( modelFPL100NoBaseline_AnovaDF, digits = c( 0, 2, 2, 0, 2, 4 ) , 
-               caption = "Model 2: FPL100 vs demographic factors, time and disability with baseline correction", 
-               floating = TRUE, latex.environments = "center"
-) )
-
-#######################################################################
 ## Print summaries of the two models
 ########################################################################
 # source( paste( RScriptPath, 'stargazer_lme4.R', sep='' ) )
-# stargazer( modelFPL100NoBaseline_Summary$coefficients, modelFPL100_Summary$coefficients, type = 'latex' )
-summaryNoBaseline <- round( modelFPL100NoBaseline_Summary$coefficients[,c( "Estimate", "Std. Error", "Pr(>|t|)" )], 4 )
+# stargazer( modelFPL100_Summary$coefficients, modelFPL100_Summary$coefficients, type = 'latex' )
 summaryWithBaseline <- round( modelFPL100_Summary$coefficients[,c( "Estimate", "Std. Error", "Pr(>|t|)" )], 4 )
 
 print( summaryWithBaseline )
-
-print( summaryNoBaseline )
 
 #######################################################################
 ## Model with Disabled only
 ########################################################################
 DataDisb <- subset( Data, adult_disb == "yes" )
 
-# FULLmodelFPL100NoBaselineDisab <- lmerTest::lmer( 
-#   FPL100_noBaseline ~ 1 + Time + I( Time^2 ) + gender + ms + race_origin + education + 
+# FULLmodelFPL100Disab <- lmerTest::lmer( 
+#   FPL100_ ~ 1 + Time + I( Time^2 ) + gender + ms + race_origin + education + 
 #     gender*ms + gender*race_origin + gender*education +
 #     ms*race_origin + ms*education + race_origin*education +
 #     ( 1 | hhid ), data = DataDisb, weights = wt
 # )
-# finalModel <- lmerTest::step( model = FULLmodelFPL100NoBaselineDisab )
-modelFPL100NoBaselineDisab <- lmerTest::lmer( 
-  FPL100_noBaseline ~ 1 + Time + I( Time^2 ) + gender + ms + race_origin + education + 
+# finalModel <- lmerTest::step( model = FULLmodelFPL100Disab )
+modelFPL100Disab <- lmerTest::lmer( 
+  FPL100 ~ 1 + Time + I( Time^2 ) + gender + ms + race_origin + education + 
     gender*ms + ms*race_origin + ms*education + race_origin*education +
     ( 1 | hhid ), data = DataDisb, weights = wt
 )
 
-modelFPL100NoBaselineDisab_Anova <- lmerTest::anova( modelFPL100NoBaselineDisab )
-modelFPL100NoBaselineDisab_Summary <- lmerTest::summary( modelFPL100NoBaselineDisab )
-print( modelFPL100NoBaselineDisab_Summary )
+modelFPL100Disab_Anova <- lmerTest::anova( modelFPL100Disab )
+modelFPL100Disab_Summary <- lmerTest::summary( modelFPL100Disab )
+print( modelFPL100Disab_Summary )
 
-modelFPL100NoBaselineDisab_AnovaDF <- formatAnovaTableForXtable( anovaTable = modelFPL100NoBaselineDisab_Anova )
-print( xtable( modelFPL100NoBaselineDisab_AnovaDF, digits = c( 0, 2, 2, 0, 2, 4 ) , 
+modelFPL100Disab_AnovaDF <- formatAnovaTableForXtable( anovaTable = modelFPL100Disab_Anova )
+print( xtable( modelFPL100Disab_AnovaDF, digits = c( 0, 2, 2, 0, 2, 4 ) , 
                caption = "Model 3: FPL100 vs demographic factors, time and disability, disability only", 
                floating = TRUE, latex.environments = "center"
 ) )
@@ -210,15 +165,15 @@ print( xtable( modelFPL100NoBaselineDisab_AnovaDF, digits = c( 0, 2, 2, 0, 2, 4 
 ########################################################################
 DataNoDisb <- subset( Data, adult_disb == "no" )
 
-modelFPL100NoBaselineNoDisab <- lmerTest::lmer( 
-  FPL100_noBaseline ~ 1 + Time + I( Time^2 ) + gender + ms + race_origin + education + 
+modelFPL100NoDisab <- lmerTest::lmer( 
+  FPL100 ~ 1 + Time + I( Time^2 ) + gender + ms + race_origin + education + 
     gender*ms + ms*race_origin + ms*education + race_origin*education +
     ( 1 | hhid ), data = DataNoDisb, weights = wt
 )
-modelFPL100NoBaselineNoDisab_Anova <- lmerTest::anova( modelFPL100NoBaselineNoDisab )
-modelFPL100NoBaselineNoDisab_Summary <- lmerTest::summary( modelFPL100NoBaselineNoDisab )
-print( modelFPL100NoBaselineNoDisab_Anova )
-print( modelFPL100NoBaselineNoDisab_Summary )
+modelFPL100NoDisab_Anova <- lmerTest::anova( modelFPL100NoDisab )
+modelFPL100NoDisab_Summary <- lmerTest::summary( modelFPL100NoDisab )
+print( modelFPL100NoDisab_Anova )
+print( modelFPL100NoDisab_Summary )
 
 #######################################################################
 ## Post hoc tests
@@ -248,34 +203,18 @@ for( Factor in postHocFactors ){
     which.plot = 'DIFF of LSMEANS', 
     mult = TRUE
   ) )
-  
-  postHocNoBaseline <- lmerTest::difflsmeans( 
-    model = modelFPL100NoBaseline, 
-    test.effs = Factor
-  )
-  
-  plotPostHocNoBaseline <- try( plotLSMeans( 
-    response = postHocNoBaseline$response,
-    table = postHocNoBaseline$diffs.lsmeans.table, 
-    which.plot = 'DIFF of LSMEANS', 
-    mult = TRUE
-  ) )
-  
+    
   CAPTION <- paste( 'Post-hoc test of', Factor )
-  postHocMerged <- try( mergePostHocTables( postHoc1 = postHoc, postHoc2 = postHocNoBaseline ) )
-  postHocMerged <- postHocMerged[ order( postHocMerged$`p-value 2`, postHocMerged$`p-value 1`, 
-                                         decreasing = F ), ]
-  
-  try( print( xtable( postHocMerged, digits = c( 0, 0, 2, 2, 4, 2, 2, 4 ), 
-                      caption = CAPTION ), include.rownames = FALSE ), )
+  columnsToDisplay <- c( 'Factor Levels', 'Estimate', 'Standard Error', 't-value', 'p-value' )  
+  postHocDF <- formatPostHocTables( postHocTable = postHoc )
+  try( print( xtable( postHocDF[, columnsToDisplay], align = 'llrrrr', digits = c( 0, 0, 2, 2, 2, 4 ), 
+                      caption = CAPTION ), include.rownames = FALSE ) )
   
   try( print( postHoc ) )
-  try( print( postHocNoBaseline ) )
   
   # try( plot( plotPostHoc ) )
   # try( plot( plotPostHocNoBaseline ) )
-  print( "#############################################################" )  
-  try( rm( postHoc, postHocNoBaseline, plotPostHoc, plotPostHocNoBaseline ) )
+  try( rm( postHoc, plotPostHoc ) )
 }
 
 # dev.off( )
@@ -298,28 +237,28 @@ for( Factor in postHocFactors ){
   print( Factor )
   print( "#############################################################" )
   
-  postHocNoBaseline <- lmerTest::difflsmeans( 
-    model = modelFPL100NoBaselineDisab, 
+  postHoc <- lmerTest::difflsmeans( 
+    model = modelFPL100Disab, 
     test.effs = Factor
   )
   
-  plotPostHocNoBaseline <- try( plotLSMeans( 
-    response = postHocNoBaseline$response,
-    table = postHocNoBaseline$diffs.lsmeans.table, 
+  plotPostHoc <- try( plotLSMeans( 
+    response = postHoc$response,
+    table = postHoc$diffs.lsmeans.table, 
     which.plot = 'DIFF of LSMEANS', 
     mult = TRUE
   ) )
   
   CAPTION <- paste( 'Post-hoc test of', Factor )
   columnsToMerge <- c( 'Factor Levels', 'Estimate', 'Standard Error', 't-value', 'p-value' )  
-  postHocNoBaselineDF <- formatPostHocTables( postHocTable = postHocNoBaseline )
-  try( print( xtable( postHocNoBaselineDF[, columnsToMerge], digits = c( 0, 0, 2, 2, 2, 4 ), 
-                      caption = CAPTION ), include.rownames = FALSE ), )
+  postHocDF <- formatPostHocTables( postHocTable = postHoc )
+  try( print( xtable( postHocDF[, columnsToMerge], align = 'llrrrr', digits = c( 0, 0, 2, 2, 2, 4 ), 
+                      caption = CAPTION ), include.rownames = FALSE ) )
   
-  try( print( postHocNoBaseline ) )
+  try( print( postHoc ) )
   
   print( "#############################################################" )
   
-  try( rm( postHocNoBaseline, plotPostHocNoBaseline ) )
+  try( rm( postHoc, plotPostHoc ) )
 }
 
