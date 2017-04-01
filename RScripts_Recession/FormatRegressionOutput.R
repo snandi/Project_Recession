f90312a909872954d74565afd0e90a28d043376e
snandi <nandi@stat.wisc.edu>
2017-03-31 19:50:54 -0500

    Updated tables with 3 dec places; sent v2 to honey; working on results

--- RScripts_Recession/FormatRegressionOutput.R
+++ RScripts_Recession/FormatRegressionOutput.R
@@ -35,7 +35,7 @@ modelFPL100_Summary <- loadModel( modelFilename = 'modelFPL100_Summary.RData' )
 Coeff_Fixed <- as.data.frame( modelFPL100_Summary$coefficients[, c( 1, 2, 5 )] )
 colnames( Coeff_Fixed ) <- c( 'beta', 'stdError', 'pValue' )
 Coeff_Fixed <- within( data = Coeff_Fixed, {
-  beta = round( beta, 2 )
+  beta = round( beta, 3 )
   stdError = round( stdError, 3 )
   pValue = round( pValue, 4 )
 })
@@ -71,5 +71,44 @@ rownames( Coeff_Fixed ) <- c(
   'Race4 x Education3'
   )
 
-print( xtable( Coeff_Fixed, align = "lrrr", digits = c( 0, 2, 3, 4), caption = "Regression result", 
+print( xtable( Coeff_Fixed, align = "lrrr", digits = c( 0, 3, 3, 4), caption = "Regression result", 
                label = "tab:Table2Reg" ), table.placement = "H" )
+
+#####################################################################
+## Disability only 
+#####################################################################
+modelFPL100Disab_Summary <- loadModel( modelFilename = 'modelFPL100Disab_Summary.RData' )
+
+Coeff_Disab <- as.data.frame( modelFPL100Disab_Summary$coefficients[, c( 1, 2, 5 )] )
+colnames( Coeff_Disab ) <- c( 'beta', 'stdError', 'pValue' )
+Coeff_Disab <- within( data = Coeff_Disab, {
+  beta = round( beta, 3 )
+  stdError = round( stdError, 3 )
+  pValue = round( pValue, 4 )
+})
+rownames( Coeff_Disab ) <- c(
+  'Intercept', 
+  'Time', 
+  'Time2', 
+  'Gender: (Female)',
+  'Marital status: (Not married)',
+  'Race2: (Black)', 
+  'Race3: (Hispanic)', 
+  'Race4: (Others)', 
+  'Education2: (Some college, diploma, assoc)', 
+  'Education3: (High School or less)',
+  'Gender x Marital status',
+  'Marital status x Race2', 
+  'Marital status x Race3', 
+  'Marital status x Race4', 
+  'Marital status x Education2', 
+  'Marital status x Education3', 
+  'Race2 x Education2', 
+  'Race3 x Education2', 
+  'Race4 x Education2', 
+  'Race2 x Education3', 
+  'Race3 x Education3', 
+  'Race4 x Education3'
+)
+print( xtable( Coeff_Disab, align = "lrrr", digits = c( 0, 3, 3, 4), caption = "Regression result", 
+               label = "tab:Table3Reg" ), table.placement = "H" )
